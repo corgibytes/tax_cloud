048c07e8bdbe9b19e493afc11c055ca42c209dc4
Chris C <chris.cumming@saturdaymp.com>
2019-03-07 13:03:50 -0700

    Added check if .gem file already exists when creating RubyGems credential file.

--- .circleci/setup-rubygems.sh
+++ .circleci/setup-rubygems.sh
@@ -4,6 +4,6 @@
 # when doing the CI build.  Assumes the CI build has
 # a RUBYGEMS_API_KEY value setup.
 
-mkdir ~/.gem
+mkdir -p ~/.gem
 echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
 chmod 0600 /home/circleci/.gem/credentials
