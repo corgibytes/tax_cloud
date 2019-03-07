3d38c4f26b50eef4006470ca6968ae116edb101f
Chris C <chris.cumming@saturdaymp.com>
2019-03-07 12:37:54 -0700

    Reports are generated locally so hopefully this works.

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,11 @@ require 'minitest/autorun'
 require 'tax_cloud'
 require 'json'
 
+# If not included then no test reports are generated
+# for Circle CI builds.
+require 'minitest/ci'
+
+
 Savon.configure do |config|
   config.log = false
 end
