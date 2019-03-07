0bb6073fd2f06aad8a45aeefcc7fd96d24b8bb48
Chris C <chris.cumming@saturdaymp.com>
2019-03-07 12:56:06 -0700

    First try at creating the actual gem.

---
+++ .circleci/setup-rubygems.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Store the RubyGems password in the credentials file
+# when doing the CI build.  Assumes the CI build has
+# a RUBYGEMS_API_KEY value setup.
+
+mkdir ~/.gem
+echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+chmod 0600 /home/circleci/.gem/credentials
